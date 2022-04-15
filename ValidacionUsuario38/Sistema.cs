7fc40a9c7fab948b1aa00be448d0f7213fa9a308
JoelCAI <joelargentina12345@gmail.com>
2022-04-15 17:06:02 -0300

    Agregar archivos de proyecto.

---
+++ ValidacionUsuario38/Sistema.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidacionUsuario38
+{
+    public class Sistema
+    {
+        public void MenuPrincipal()
+        {
+            Validador.Bienvenida();
+
+            int numeroUno = Validador.PedirInt("\n Ingrese un número");
+
+            Console.Clear();
+            Console.WriteLine("\n El número ingresado fue *" + numeroUno + "*.");
+
+            int numeroDos = Validador.PedirIntComparar("\n Ingrese un número",numeroUno);
+
+            Console.Clear();
+            Console.WriteLine("\n El siguiente número ingresado fue *" + numeroDos + "*.");
+
+            int numeroTres = Validador.PedirIntComparar("\n Ingrese un número", numeroDos);
+
+            Console.Clear();
+            Console.WriteLine("\n Los números ingresados en orden ascendente son: " + 
+                              "\n Número *" + numeroUno + "* como primero" +
+                              "\n Número *" + numeroDos + "* como segundo" +
+                              "\n Número *" + numeroTres + "* como tercero");
+
+            Validador.Despedida();
+
+        }
+
+        public void Iniciar()
+        {
+            MenuPrincipal();
+        }
+    }
+}
---
+++ ValidacionUsuario38/Validador.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidacionUsuario38
+{
+    public class Validador
+    {
+        public static int PedirInt(string mensaje)
+        {
+            /* la salida de esta funcion estará en la variable int valor */
+            string mensError = "\n\n Por favor ingrese el valor solicitado" +
+                               "\n Debe ingresar un valor que no sea vacio y que ese valor sea un número. ";
+
+            int valor;
+            Console.WriteLine(mensaje);
+
+            if (!int.TryParse(Console.ReadLine(), out valor))
+            {
+                Console.Clear();
+                Console.WriteLine(mensError);
+                return PedirInt(mensaje);
+            }
+            else
+                return valor;
+        }
+
+        public static int PedirIntComparar(string mensaje, int numeroAnterior)
+        {
+            /* la salida de esta funcion estará en la variable int valor */
+            string mensError = "\n\n Por favor ingrese el valor solicitado" +
+                               "\n Debe ingresar un valor que no sea vacio y que ese valor sea mayor al número anterior " +
+                               "ingresado. ";
+
+            
+            int valor;
+            string cadena;
+            Console.WriteLine(mensaje);
+            Console.WriteLine(" El número anterior que ingreso es: " + numeroAnterior);
+            cadena = Console.ReadLine();
+
+            if (!int.TryParse(cadena, out valor))
+            {
+                Console.Clear();
+                Console.WriteLine(mensError);
+                return PedirIntComparar(mensaje,numeroAnterior);
+            }
+
+            int numero; 
+            bool ok = int.TryParse(cadena, out numero);
+            if (numeroAnterior >= numero )
+            {
+                Console.Clear();
+                Console.WriteLine(mensError);
+                return PedirIntComparar(mensaje, numeroAnterior);
+            }
+            else
+                return valor;
+        }
+
+        public static void Bienvenida()
+        {
+            Console.WriteLine("\n Bienvenido al Programa");
+        }
+
+        public static void Despedida()
+        {
+
+            Console.WriteLine("\n Gracias por usar nuestro Sistema presione cualquier teclar para finalizar");
+            Console.ReadKey();
+        }
+    }
+}
